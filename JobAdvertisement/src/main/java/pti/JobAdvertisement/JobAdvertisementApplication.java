8ab12d735035e834fb1d64ee64134bb7b7454888
bogdanovTeam7 <bogdanov@t-email.hu>
2022-12-20 16:44:53 +0100

    init

---
+++ JobAdvertisement/src/main/java/pti/JobAdvertisement/JobAdvertisementApplication.java
@@ -0,0 +1,13 @@
+package pti.JobAdvertisement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAdvertisementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAdvertisementApplication.class, args);
+	}
+
+}
---
+++ JobAdvertisement/src/test/java/pti/JobAdvertisement/JobAdvertisementApplicationTests.java
@@ -0,0 +1,13 @@
+package pti.JobAdvertisement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAdvertisementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
